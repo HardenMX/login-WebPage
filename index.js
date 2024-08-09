6b1e820f6d37e910e06295f606993a0a200a242f
Fmorejon <morejonff@gmail.com>
2024-08-08 22:23:07 -0600

    needs to validate data

---
+++ index.js
@@ -0,0 +1,9 @@
+const submitBtn = document.getElementById('btn-submit');
+const userEmail = document.getElementById('email').textContent;
+const userPassword = document.getElementById('password').textContent;
+
+const submitFunction = (userEmail, userPassword) => {
+    console.log(userEmail, userPassword);
+}
+
+
